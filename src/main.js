3a8371b971e9e722610494c3f223f5d99c00ad10
fiofernandini <fiofernandini12@gmail.com>
2019-10-29 09:24:23 -0500

    agrgando

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import POKEMON from './data/pokemon/pokemon.js'
+import POKEMON from './data/pokemon/pokemon.js';
 import { mostrarPokemon } from './data.js';
 
 document.querySelector('#insertar-pokemones').innerHTML = mostrarPokemon(POKEMON);
