6e53b150820aea9b74f4ff173d79bc428dd55acb
PaolaS2992 <huaripayta29.92@gmail.com>
2019-11-04 10:40:37 -0500

    Modal

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 export const mostrarPokemon = (datos) => {
   let contenedorPokemon = '';
-  datos.map((elementos)=>{
+  datos.forEach((elementos) => {
     contenedorPokemon += `
-    <div class="tarjeta-pokemon">
+    <div class="tarjeta-pokemon" id=${elementos.id}>
       <img src="${elementos.img}" class="imagen-pokemon">
       <p class="nombre-pokemon">${elementos.name}</p>
       <p class="numero-pokemon">${elementos.num}</p>
