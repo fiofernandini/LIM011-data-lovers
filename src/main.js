378234e4097275b96691292445fce03a4eb4872b
PaolaS2992 <huaripayta29.92@gmail.com>
2019-11-11 13:55:12 -0500

    Appendchild - Betsy

--- src/main.js
+++ src/main.js
@@ -3,69 +3,77 @@ import {
   traerDataMap2, filtroHuevo, filtroDebilidadTipo, buscarPorNombre, ordenAlfNum, AsDes
 } from './data.js';
 
-
-const dataPokemon = traerDataMap2(POKEMON);
-const templateCard = (arr) => {
-  let stringTemplate = '';
-  arr.forEach((obj) => {
-    stringTemplate += `<div class="tarjeta-pokemon" id=${obj.identificador}>
-    <img src="${obj.imagen}" class="imagen-pokemon">
-    <p class="nombre-pokemon"  >${obj.nombre}  </p>
-    <p class="numero-pokemon" >${obj.numero} </p>
-    <div class="informacion-alternativa">
-    <div class="datos-alternativos">Altura: ${obj.altura}</div>
-    <div class="datos-alternativos">Peso: ${obj.peso}</div>
-    <div class="datos-alternativos">Caramelos: ${obj.caramelos}</div>
-    <div class="datos-alternativos">Hora de aparación: ${obj.horaAparicion}</div>
-    <div class="datos-alternativos"}>Debilidades:</div>
-    <div class="datos-alternativosD">${obj.debilidades}</div>
-    <div class="huevo-pokemon"><img src="./imagenes/huevo.png" width="15" height"15">${obj.huevo}</div>
-    </div>
-    </div>`;
-  });
-  return stringTemplate;
-};
+const card = (obj) => {
+  const divElement = document.createElement('div');
+  divElement.classList.add("tarjeta-pokemon");
+  divElement.innerHTML = `
+  <img src="${obj.imagen}" class="imagen-pokemon">
+  <p class="nombre-pokemon"  >${obj.nombre}  </p>
+  <p class="numero-pokemon" >${obj.numero} </p>
+  <div class="informacion-alternativa">
+  <div class="datos-alternativos">Altura: ${obj.altura}</div>
+  <div class="datos-alternativos">Peso: ${obj.peso}</div>
+  <div class="datos-alternativos">Caramelos: ${obj.caramelos}</div>
+  <div class="datos-alternativos">Hora de aparación: ${obj.horaAparicion}</div>
+  <div class="datos-alternativos"}>Debilidades:</div>
+  <div class="datos-alternativosD">${obj.debilidades}</div>
+  </div>
+  `
+  divElement.addEventListener('click', () => {
+    const pElement = document.createElement('p');
+    pElement.innerHTML = `<div class="huevo-pokemon"><img src="./imagenes/huevo.png" width="15" height"15">${obj.huevo}</div>`
+    divElement.appendChild(pElement);
+  })
+  return divElement;
+}
 
 const seccionCardsPokemones = document.querySelector('#insertar-pokemones');
-seccionCardsPokemones.innerHTML = templateCard(dataPokemon);
+const dataPokemon = traerDataMap2(POKEMON);
+const templateCard = (arr) => {
+  seccionCardsPokemones.innerHTML = '';
+  arr.forEach((obj) => {
+    seccionCardsPokemones.appendChild(card(obj));
+  });
+};
+
+templateCard(dataPokemon);
 
 // huevos
 document.querySelector('#filtro-distancia').addEventListener('change', () => {
   const seleccionarhuevo = document.querySelector('#filtro-distancia').value;
-  seccionCardsPokemones.innerHTML = templateCard(filtroHuevo(dataPokemon, seleccionarhuevo));
+  templateCard(filtroHuevo(dataPokemon, seleccionarhuevo));
 });
 // debilidades
 document.querySelector('#filtro-debilidades').addEventListener('change', () => {
   const seleccionarDebilidad = document.querySelector('#filtro-debilidades').value;
   console.log(seleccionarDebilidad);
-  seccionCardsPokemones.innerHTML = templateCard(filtroDebilidadTipo(dataPokemon, 'debilidad' ,seleccionarDebilidad));
-  console.log(templateCard(filtrodebilidad(dataPokemon, seleccionarDebilidad)));
+  templateCard(filtroDebilidadTipo(dataPokemon, 'debilidades' ,seleccionarDebilidad));
 });
 
 // 003: FILTRO POR ORDEN ALFABETICO Y NUMERICO.
 document.querySelector('#ordenAlfNum').addEventListener('change', ()=>{
   const seleccionOpcion = document.querySelector('#ordenAlfNum').value;
   console.log(seleccionOpcion);
-  seccionCardsPokemones.innerHTML = templateCard(AsDes(dataPokemon, seleccionOpcion));
+  templateCard(AsDes(dataPokemon, seleccionOpcion));
   console.log(ordenAlfNum(dataPokemon, seleccionOpcion));
 });
 
 // Tipo
 document.querySelector('#guia-tipos').addEventListener('click', (event) => {
   const seleccionarTipo = event.target.alt;
-  seccionCardsPokemones.innerHTML = templateCard(filtroDebilidadTipo(dataPokemon, 'tipo' , seleccionarTipo));
+  templateCard(filtroDebilidadTipo(dataPokemon, 'tipo' , seleccionarTipo));
 });
 
 // nombre
 document.querySelector('#nombre-pokemon').addEventListener('input', (event) => {
   const pokemonBuscado = event.target.value.toLowerCase();
-  seccionCardsPokemones.innerHTML = templateCard(buscarPorNombre(dataPokemon, pokemonBuscado));
+  templateCard(buscarPorNombre(dataPokemon, pokemonBuscado));
 });
 
 // 005: TOP 10 DE FRECUENCIA DE APARICIÓN.
 const btnTop10 = document.querySelector('#mayor-frecuencia');
 btnTop10.addEventListener('click',() => {
-    seccionCardsPokemones.innerHTML = templateCard(AsDes(dataPokemon, btnTop10.value));
+  templateCard(AsDes(dataPokemon, btnTop10.value));
 });
 
 // menu
